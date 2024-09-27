039b1b3eb643bae43b456af89394b67f88d26f2a
santosbat <149489305+santosbat@users.noreply.github.com>
2024-09-27 11:15:08 +0000

    p

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Description from './pages/Description';
-import Points from './pages/Points';
-import Partners from './pages/Partners';
+import Home from './pages/home/Home';
+import Login from './pages/Login/Login';
+import Signup from './pages/Cadastro/Signup';
+import Description from './pages/Descricao/Description';
+import Points from './pages/Pontos/Points';
+import Partners from './pages/Parceiros/Partners';
 import './App.css'
 
 function App() {
